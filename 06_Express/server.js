ebf79ca18c629470842a9e499d1ecc27c8f3d81d
Sandipan Ghorai <sandipan.ghorai93@gmail.com>
2022-12-19 18:43:07 +0530

    Commit Chapter 6

--- 06_Express/server.js
+++ 06_Express/server.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+const cors = require('cors');
+
 const PORT = process.env.PORT || 3000;
 
-const ERROR_LOG = 'errLog.txt';
-const REQUEST_LOG = 'reqLog.txt';
+//Custom Middleware Logger
+app.use(logger);
+
+const whiteList = ['https://www.google.com', 'http://127.0.0.1:5500', 'http://localhost:3000'];
+const corsOption = {
+    origin: (origin, callback) => {
+        if(whiteList.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOption));
 
 /**
  * Built-in middleware to handle urlencoded data.
@@ -58,10 +76,22 @@ const three = (req, res) => {
 };
 app.get('/chain(.html)?', [one, two, three]);
 
+/*
 app.get('/*', (req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
+*/
+app.all('*', (req, res) => {
+    res.status(404);
+    if(req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if(req.accepts('json')) {
+        res.json({ error : "404 Not Found"});
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
+});
 
-
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Web Server is running on port : ${PORT}`));
